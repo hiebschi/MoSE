1c425769279434c97e67bb4b186445b3532bc1a3
hiebschi <simon.hiebsch@web.de>
2025-01-04 13:02:50 +0100

    helper functions

--- scripts/data_utils.py
+++ scripts/data_utils.py
@@ -10,14 +10,45 @@ If a function gets defined once and could be used over and over, it'll go in her
 
 import os
 
-import numpy as np
-import geopandas as gpd
+# import numpy as np
+# import geopandas as gpd
 
-import sklearn
+# import sklearn
 
-import matplotlib.pyplot as plt
+# import matplotlib.pyplot as plt
 
-import torch
-import torchvision
-from torch import nn
+# import torch
+# import torchvision
+# from torch import nn
+
+
+def extract_section_and_id(file_name):
+    """
+    Extracts section and patch_id from the file_name of masks and of preprocessed and compressed patches.
+    
+    Args:
+        file_name (str): file name of an preprocessd patch (.npy.npz) or an mask (_mask.npy)
+    
+    """
+    
+    parts = file_name.split("_") # split condition: _
+    section = parts[0]  # extract section from file_name, e.g. "A01"
+    patch_id = parts[2].replace(".npy.npz", "").replace("_mask", "") #  extract patch_id, e.g. 481
+    
+    return section, patch_id
+
+
+def has_mask(patch_name, masks_dir):
+    """ 
+    Check if a patch has a corresponding mask in the masks directory.
+
+    Args:
+        patch_name (str): Name of the patch (e.g., 'A01_patch_481.npy.npz').
+        masks_dir (str): Directory where masks are stored.
+    """
+
+    mask_path = os.path.join(masks_dir, patch_name.replace(".npy.npz", "_mask.npy")) # load corresponding mask path
+    
+    return os.path.exists(mask_path) # check if path exists
+
 
--- scripts/visualization_utils.py
+++ scripts/visualization_utils.py
@@ -6,3 +6,32 @@ of the segmentation model implementation.
 
 If a function gets defined once and could be used over and over, it'll go in here.
 """
+
+import numpy as np
+import matplotlib.pyplot as plt
+import math
+
+
+
+def norm_plot_patch(patch):
+    """
+    Normalizes and plots a single preprocessed patch.
+    
+    Args:
+        patch (numpy.ndarray): preprocessed patch/ image data
+    
+    Returns: 
+        Plot of normalized image
+    """
+
+    # Normalization
+    patch_normalized = patch - np.min(patch ) # set minimum to 0
+    patch_normalized = patch_normalized / np.max(patch_normalized)  # maximize to 1
+
+    # Plot the preprocessed image
+    fig, ax = plt.subplots(figsize=(8, 8))
+    ax.imshow(patch_normalized.transpose(1, 2, 0))  # transpose for RGB depiction
+    ax.set_title("Preprocessed Patch")
+    plt.show()
+
+
